847d94c7782dad155c30434e670ed6d21b4bffd7
marciales34 <diegomarciales06@gmail.com>
2025-03-07 12:17:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TomarFotosIonic',
+  webDir: 'www'
+};
+
+export default config;
